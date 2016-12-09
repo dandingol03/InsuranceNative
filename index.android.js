3e856587009dc5806a60b6fbca33cbb89349a082
danding <1291248710@qq.com>
2016-12-09 16:03:13 +0800

    xx

--- index.android.js
+++ index.android.js
@@ -6,48 +6,62 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
-export default class InsuranceNative extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+import App from './App/index';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import  TabNavigator from 'react-native-tab-navigator';
+
+import ScrollableTabView,{DefaultTabBar} from 'react-native-scrollable-tab-view';
+
+export default class InsuranceNative extends  Component{
+    constructor(props)
+    {
+        super(props);
+        this.state = {selectedTab: 'home'};
+    }
+
+    render(){
+        return (
+
+            <View style={{flex:1}}>
+                <Text>android scroll</Text>
+
+                <ScrollableTabView
+                    renderTabBar={() => <DefaultTabBar/>}>
+                    <Text tabLabel='Tab1'/>
+                    <Text tabLabel='Tab2'/>
+                    <Text tabLabel='Tab3'/>
+                    <Text tabLabel='Tab4'/>
+                    <Text tabLabel='Tab5'/>
+                    <Text tabLabel='Tab6'/>
+                </ScrollableTabView>
+
+            </View>
+
+        );
+    }
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
-AppRegistry.registerComponent('InsuranceNative', () => InsuranceNative);
+
+
+AppRegistry.registerComponent('InsuranceNative', () => App);
