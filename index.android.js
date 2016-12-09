b510bfeefbf4d67458d31a063767f77b2b8543fc
danding <1291248710@qq.com>
2016-12-09 16:04:05 +0800

    xx

--- index.android.js
+++ index.android.js
@@ -13,55 +13,6 @@ import {
 } from 'react-native';
 
 import App from './App/index';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import  TabNavigator from 'react-native-tab-navigator';
-
-import ScrollableTabView,{DefaultTabBar} from 'react-native-scrollable-tab-view';
-
-export default class InsuranceNative extends  Component{
-    constructor(props)
-    {
-        super(props);
-        this.state = {selectedTab: 'home'};
-    }
-
-    render(){
-        return (
-
-            <View style={{flex:1}}>
-                <Text>android scroll</Text>
-
-                <ScrollableTabView
-                    renderTabBar={() => <DefaultTabBar/>}>
-                    <Text tabLabel='Tab1'/>
-                    <Text tabLabel='Tab2'/>
-                    <Text tabLabel='Tab3'/>
-                    <Text tabLabel='Tab4'/>
-                    <Text tabLabel='Tab5'/>
-                    <Text tabLabel='Tab6'/>
-                </ScrollableTabView>
-
-            </View>
-
-        );
-    }
-}
-
-
-
-const styles = StyleSheet.create({
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
-
 
 
 AppRegistry.registerComponent('InsuranceNative', () => App);
